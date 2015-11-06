1f5b5c0e1cb2882ad48534a33489094fe536968c
Michael Weibel <michael.weibel@gmail.com>
2015-11-06 19:54:09 +0100

    Initial Commit

---
+++ ESRScanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ESRScanner
+//
+//  Created by Michael on 06.11.15.
+//  Copyright © 2015 Michael Weibel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ESRScannerTests/ESRScannerTests.swift
@@ -0,0 +1,36 @@
+//
+//  ESRScannerTests.swift
+//  ESRScannerTests
+//
+//  Created by Michael on 06.11.15.
+//  Copyright © 2015 Michael Weibel. All rights reserved.
+//
+
+import XCTest
+@testable import ESRScanner
+
+class ESRScannerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
