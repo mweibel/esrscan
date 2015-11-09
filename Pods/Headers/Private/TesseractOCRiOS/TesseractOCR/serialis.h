0f6705506e8083a492bf7ea53fc27b69ee728b46
Michael Weibel <michael.weibel@gmail.com>
2015-11-09 20:29:41 +0100

    Re-add pods

---
+++ Pods/Headers/Private/TesseractOCRiOS/TesseractOCR/serialis.h
@@ -0,0 +1,1 @@
+../../../../../../Tesseract-OCR-iOS/TesseractOCR/include/tesseract/serialis.h
---
+++ Pods/Target Support Files/Pods-ESRScanner/Pods-ESRScanner-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ESRScanner : NSObject
+@end
+@implementation PodsDummy_Pods_ESRScanner
+@end
