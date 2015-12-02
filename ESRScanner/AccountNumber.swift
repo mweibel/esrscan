b66b6518ca19c520e5b6e17c45e256410b143b93
Michael Weibel <michael.weibel@gmail.com>
2015-12-02 07:31:02 +0100

    improve display of account number

--- ESRScanner/AccountNumber.swift
+++ ESRScanner/AccountNumber.swift
@@ -15,8 +15,20 @@ class AccountNumber {
     }
 
     func string() -> String {
-        let prefix = String(num[num.startIndex.advancedBy(2)])
-        let center = num.substringWithRange(Range(start: num.startIndex.advancedBy(2), end: num.endIndex.advancedBy(-1)))
+        let prefix = num.substringWithRange(
+            Range(start: num.startIndex, end: num.startIndex.advancedBy(2)
+        ))
+        var center = num.substringWithRange(
+            Range(start: num.startIndex.advancedBy(2), end: num.endIndex.advancedBy(-1)
+        ))
+
+        let regex = try! NSRegularExpression(pattern: "^0+", options: [])
+
+        center = regex.stringByReplacingMatchesInString(center,
+            options: [],
+            range: NSMakeRange(0, center.characters.count),
+            withTemplate: ""
+        )
         let postfix = String(num[num.endIndex.advancedBy(-1)])
         return prefix + "-" + center + "-" + postfix
     }
